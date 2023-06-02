9d143b2af49b1ff67bea3b94255028db00bf24cd
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2023-06-02 21:15:15 +0200

    The timing of this thing, pure pain

--- game.js
+++ game.js
@@ -8,15 +8,12 @@ $(document).ready(function () {
   let userClickedPattern = [];
   let gameStart = false;
   let level = 0;
-
-  $(document).on("keydown mousedown touchstart", function () {
-    setTimeout(function () {
+    $(document).on("keydown mousedown touchstart", function () {
       if (!gameStart) {
         gameStart = true;
         nextSequence();
       }
-    }, 2000);
-  });
+    });
 
   $(".btn").click(function () {
     let userChosenColor = $(this).attr("id");
@@ -36,18 +33,20 @@ $(document).ready(function () {
     let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
-    $("#" + randomChosenColor).fadeTo(100, 0.3, function () {
-      $(this).fadeTo(100, 1.0);
-    });
-    playSound(randomChosenColor);
-    animatePress(randomChosenColor);
     level++;
     $("#level-title").text("Level " + level);
-    userClickedPattern = []; // reset the user clicked pattern array
+     setTimeout(function () {
+       $("#" + randomChosenColor).fadeTo(100, 0.3, function () {
+         $(this).fadeTo(100, 1.0);
+       });
+       playSound(randomChosenColor);
+       animatePress(randomChosenColor);
+       userClickedPattern = []; // reset the user clicked pattern array
 
-    // $("#" + randomChosenColor).fadeIn(1000).fadeOut(1000).fadeIn(1000); // Make button flicker depending on random color
-    // let audio = new Audio("sounds/" + randomChosenColor + ".mp3"); // Make audio depending on random color
-    // audio.play();
+       // $("#" + randomChosenColor).fadeIn(1000).fadeOut(1000).fadeIn(1000); // Make button flicker depending on random color
+       // let audio = new Audio("sounds/" + randomChosenColor + ".mp3"); // Make audio depending on random color
+       // audio.play();
+     }, 1000);
   }
 
   function checkAnswer(currentLevel) {
@@ -59,12 +58,12 @@ $(document).ready(function () {
       }
     } else {
       playSound("wrong");
-      // let audioWrong = new Audio("sounds/wrong.mp3"); 
+      // let audioWrong = new Audio("sounds/wrong.mp3");
       // audioWrong.play();
       $("body").addClass("game-over");
       setTimeout(function () {
         $("body").removeClass("game-over");
-      }, 2000);
+      }, 1000);
       // console.log("fail");
       $("#level-title").text("Game Over, Press Here to Restart");
       startOver();
